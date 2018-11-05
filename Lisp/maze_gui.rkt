2dc79bb218ca0552ef4db1988557a0baf6c2ea7c
PascalZh <1315521905@qq.com>
2018-11-05 23:26:26 +0800

    add maze_gui.rkt and its c++ interface

--- Lisp/maze_gui.rkt
+++ Lisp/maze_gui.rkt
@@ -2,7 +2,9 @@
 #lang racket/gui
 (require "libcommon.rkt")
 
-(provide set-Oxy set-size set-grid-size set-colors get-canvas)
+(provide show-maze)
+(provide set-origin set-size set-grid-size set-colors
+         set-background get-canvas)
 (provide make-colors colors-ref colors-set!)
 
 (def (paint-maze canvas dc)
@@ -29,7 +31,7 @@
         (iter_ H)
         (iter (- i 1)))))
 
-  (send dc set-brush "green" 'solid)
+  (send dc set-brush background'solid)
   (send dc draw-rectangle
         Ox Oy
         maze-width
@@ -37,46 +39,49 @@
 
   (iter W))
 
-(def H 4)
-(def W 4)
-(def Ox 10)
-(def Oy 11)
-(def grid-width 40)
-(def grid-height 40)
+(def H 10)          (def W 10)
+(def Ox 10)         (def Oy 11)
+(def grid-width 30) (def grid-height 30)
 (def grid-border (/ grid-width 10))
 
 (def maze-width (+ grid-border (* grid-width W)))
 (def maze-height (+ grid-border (* grid-height H)))
-
-(def (set-Oxy x y)
-  (set! Ox x)
-  (set! Oy y))
-
-(def (set-size w h) (set! W w) (set! H h)
-  (set! colors (make-colors W H))
-  (set! maze-width (+ grid-border (* grid-width W)))
-  (set! maze-height (+ grid-border (* grid-height H)))
-  )
-(def (set-grid-size w h) (set! grid-width w) (set! grid-height h)
-  (set! maze-width (+ grid-border (* grid-width W)))
-  (set! maze-height (+ grid-border (* grid-height H)))
-  )
 
 (def (make-colors w h)
   (build-mlist W (λ (x) (build-mlist H (λ (x) "blue")))))
 
 ; assign color for every grid
 (def colors (make-colors W H))
+(def background "cyan")
 
-(def (colors-ref clrs x y)
-  (mlist-ref (mlist-ref clrs x) y))
+(def (set-origin x y)
+  (set! Ox x)
+  (set! Oy y)
+  (refresh-maze))
+
+(def (set-size w h) (set! W w) (set! H h)
+  (set! colors (make-colors W H))
+  (set! maze-width (+ grid-border (* grid-width W)))
+  (set! maze-height (+ grid-border (* grid-height H)))
+  (refresh-maze))
+
+(def (set-grid-size w h) (set! grid-width w) (set! grid-height h)
+  (set! maze-width (+ grid-border (* grid-width W)))
+  (set! maze-height (+ grid-border (* grid-height H)))
+  (refresh-maze))
 
 (def set-colors
   (case-lambda
-    [(clrs) (cond [(mpair? clrs) (set! colors clrs)
-                                 (refresh-maze)])]
+    [(clrs) (cond [(mpair? clrs)
+                   (set! colors clrs)
+                   (refresh-maze)])]
     [(x y color) (colors-set! colors x y color)
                  (refresh-maze)]))
+
+(def (set-background bg) (set! background bg) (refresh-maze))
+
+(def (colors-ref clrs x y)
+  (mlist-ref (mlist-ref clrs x) y))
 
 (def (colors-set! clrs x y color)
   (def (iter l i)
@@ -88,12 +93,14 @@
       (iter_ (mcar l) y)
       (iter (mcdr l) (i . - . 1))))
   (iter clrs x))
+
 (def canvas 0)
 (def (get-canvas) canvas)
 (def (set-canvas cvs)
   (set! canvas cvs))
 (def (refresh-maze)
-  (send canvas refresh))
+  (when (not (eq? canvas 0))
+    (send canvas refresh)))
 
 ; test {{{
 (module+ test
@@ -105,17 +112,37 @@
   )
 ; }}}
 
-(module* main #f
+(def (show-maze)
   (def frame (new frame% [label "Maze"]
-                  [width 500]
-                  [height 600]))
+                  [width (+ Ox Ox maze-width)]
+                  [height (+ Oy Oy maze-height)]))
   (set-canvas (new canvas% [parent frame]
                    [paint-callback paint-maze]))
-  (set-size 5 5)
-  (set-Oxy 100 200)
-  (colors-set! colors 0 0 "red")
-  (colors-set! colors 3 0 "red")
-  (colors-set! colors 0 3 "red")
-  (colors-set! colors 3 3 "red")
-  (send frame show #t)
-  )
+  (send frame show #t))
+
+
+(def (racket-print x)
+  (if (void? x)
+    null
+    (begin
+      (display "Racket ]=> ")
+      (displayln x))))
+
+(define-namespace-anchor anchor)
+(define ns (namespace-anchor->namespace anchor))
+
+; use this function to communicate with other process
+(def (maze-driver-loop)
+  (let ([input (begin (display ">>") (read))])
+    (if (eof-object? input)
+      null
+      (begin
+        (cond [(pair? input)
+               (with-handlers ([any/c (λ (msg) (racket-print msg))])
+                 (racket-print (eval input ns)))]
+              [else (println input)])
+        (maze-driver-loop)))))
+
+(module* main #f
+  (show-maze)
+  (thread maze-driver-loop))
