6b8a23ca5aa9f7e5e353e3aff6e1ca3eb3c77642
PascalZh <1315521905@qq.com>
2021-11-09 18:04:05 +0800

    FPGA LED: programmed into the board

--- EE/FPGA/LED/LED.v
+++ EE/FPGA/LED/LED.v
@@ -1,30 +1,37 @@
 module LED (
-           input clk,
-           input rst,
-           output reg led
-       );
+    input clk,
+    input rst,
+    output reg led,
+    output reg beep
+  );
 
-reg [24-1:0] cnt;
+  reg [24-1:0] cnt;
 
-// initial begin
-//     cnt = 24'b0;
-// end
+  // initial begin
+  //     cnt = 24'b0;
+  // end
 
-always @(posedge clk, posedge rst) begin
-    if (rst)
-        cnt <= 24'd0;
-    else if (cnt == 1249999)
-        cnt <= 24'd0;
+  always @(posedge clk, negedge rst) begin
+    if (!rst)
+      cnt <= 24'd0;
+    else if (cnt == 15_249_999)
+      cnt <= 24'd0;
     else
-        cnt <= cnt + 24'b1;
-end
+      cnt <= cnt + 24'b1;
+  end
 
-// 控制LED循环闪动
-always @(posedge clk, posedge rst) begin
-    if (rst)
-        led <= 1'd0;
-    else if (cnt == 1249999)
-        led <= ~led;
-end
+  // 控制LED循环闪动
+  always @(posedge clk, negedge rst) begin
+    if (!rst) begin
+      led <= 1'd1;
+      beep <= 1'd0;
+    end else if (cnt == 15_249_999) begin
+      led <= ~led;
+    end
+
+    if (cnt % 10000 == 0) begin
+      beep <= ~beep;
+    end
+  end
 
 endmodule
